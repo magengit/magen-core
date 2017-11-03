26eb4590a28aef000bab6248f11ee1be7dc158a1
usetheapi <lifar_tut_net@mail.ru>
2017-11-03 11:14:51 +0300

    [test] clean up

--- magen_id_client/tests/magen_id_client_test.py
+++ magen_id_client/tests/magen_id_client_test.py
@@ -89,8 +89,6 @@ class MagenIdClientTestCase(unittest.TestCase):
 
         # Verifying the whole scenario was executed correctly
         self.assertTrue(redirect_response.success)
-        # self.assertEqual(redirect_response.http_status, HTTPStatus.OK)
-        # self.assertEqual(redirect_response.json_body, requests_mock.return_value.json.return_value)
         # Assertion of expected url passed to flask.redirect()
         redirect_mock.assert_called_once_with(expected_url)
 
