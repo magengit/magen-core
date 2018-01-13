257a73acc5617e77ef0edf6b9350e33b408cd677
usetheapi <lifar_tut_net@mail.ru>
2018-01-13 16:10:15 +0300

    [feat] [CM-754] [CM-811] e-mail confirmation workflow implementation + html email template

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ Config file. Global constants
 TEST_DB_NAME = 'test_magen_demo'
 DEV_DB_NAME = 'dev_magen_demo'
 PROD_DB_NAME = 'magen_demo'
+AUTO_SENDER = 'no-reply@magen.io'
 
 USER_COLLECTION_NAME = 'users'
 
--- user.py
+++ user.py
@@ -2,7 +2,7 @@
 """
 User blah blah
 """
-
+import datetime
 from flask import Flask, Blueprint, request, render_template, flash, url_for, redirect
 from flask_login import LoginManager, login_required, login_user
 from flask_wtf import FlaskForm
@@ -14,7 +14,9 @@ import hashlib
 
 import db
 from user_model import UserModel, generate_salt
-from config import DEV_DB_NAME, EXISTING_EMAIL_CODE_ERR, USER_COLLECTION_NAME
+from config import DEV_DB_NAME, EXISTING_EMAIL_CODE_ERR, AUTO_SENDER
+
+from magen_gmail_client_api import gmail_client
 
 # creating blueprints
 users_bp = Blueprint('users_bp', __name__)
@@ -112,15 +114,26 @@ def register():
                                                 salt.encode('utf-8'), ITERATIONS)
             password = hashlib.pbkdf2_hmac(HASH_FUNCTION, hash_password,
                                            salt.encode('utf-8'), ITERATIONS).hex()
-            user_details = dict(
-                confirmed=False
-            )
+            # Here goes additional stuff from the form
+            user_data = {}
             with db.connect(DEV_DB_NAME) as db_instance:
-                user = UserModel(db_instance, email, password, salt, **user_details)
+                user = UserModel(db_instance, email, password, salt, **user_data)
                 result = user.submit()
             if result.success:
-                # TODO (for Alena): email generation
-                # token = generate_confirmation_token(email)
+                token = generate_confirmation_token(email)
+                confirm_url = url_for('users_bp.confirm_email', token=token, _external=True)
+                html = render_template('email_confirmation.html', confirm_url=confirm_url, user_email=email)
+
+                msg = gmail_client.create_message(
+                    sender=AUTO_SENDER,
+                    to=email,
+                    subject='User Registration Confirmation',
+                    text_part='Please confirm your e-mail address',
+                    html_part=html
+                )
+
+                with gmail_client.connect() as gmail_service:
+                    gmail_client.send_message(gmail_service, 'me', msg)
                 flash('A confirmation email has been sent via email.', 'success')
                 return redirect(url_for('main_bp.home'))
             else:
@@ -135,14 +148,12 @@ def register():
 def login():
     """ Login for the user by email and password provided to Login Form """
     form = LoginForm(request.form)
-    # user = dict()
     if request.method == 'POST':
         if form.validate_on_submit():
             with db.connect(DEV_DB_NAME) as db_instance:
                 result = UserModel.select_by_email(db_instance, str(form.email.data))
             if result.count:
                 user = result.documents
-                # FIXME (for Alena): correct hashing and verifying
                 if check_password_hash(user.password, user.salt, form.password.data.encode('utf-8')):
                     login_user(user)
                     flash('Welcome.', 'success')
@@ -163,6 +174,32 @@ def login():
 def home():
     """Index page"""
     return render_template('index.html')
+
+
+@users_bp.route('/confirm/<token>')
+def confirm_email(token):
+    """
+
+    :param token:
+    :return:
+    """
+    email = confirm_token(token)
+    if not email:
+        flash('Confirmation link is invalid or has expired')
+        return redirect(url_for('main_bp.home'))
+    with db.connect(db_name=DEV_DB_NAME) as db_instance:
+        result = UserModel.select_by_email(db_instance, email)
+    if result.count:
+        user = result.documents
+        if user.confirmed:
+            flash('User is already confirmed, please login')
+            return redirect(url_for('user_bp.login'))
+        user._is_authenticated = True
+        user.confirmed = True
+        user.confirmed_on = datetime.datetime.now()
+        user.submit()
+        flash('You have confirmed your account. Thanks!', 'success')
+    return redirect(url_for('main_bp.home'))
 
 
 @login_manager.user_loader
