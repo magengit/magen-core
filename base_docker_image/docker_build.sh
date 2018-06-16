85df1bb9ff79d00f92d70e612bd7e763fbc0984d
repenno <rapenno@gmail.com>
2018-06-16 14:35:30 -0700

    Removing magen_user and gmail_client from docker build

--- base_docker_image/docker_build.sh
+++ base_docker_image/docker_build.sh
@@ -12,9 +12,9 @@ requirements_filename=magen_requirements.txt
 number_of_packages=9
 
 # names of folders where magen packages are stored
-folders=(magen_logaru magen_utils magen_datastore magen_rest magen_stats magen_mongo magen_id_client magen_gmail_client magen_user)
+folders=(magen_logaru magen_utils magen_datastore magen_rest magen_stats magen_mongo magen_id_client)
 # names of magen packages
-packages=(magen_logger magen_utils magen_datastore magen_rest_service magen_statistics_service magen_mongo magen_id_client magen_gmail_client magen_user)
+packages=(magen_logger magen_utils magen_datastore magen_rest_service magen_statistics_service magen_mongo magen_id_client)
 
 for ((i=0; i<${number_of_packages}; i++))
 do
